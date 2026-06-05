f837151efde3642c367f63f815bf2cc1898644ea
echosandesh <echosandeshio@gmail.com>
2026-06-05 07:39:10 +0000

    suggestted fix

--- src/main/java/org/springframework/samples/petclinic/vet/WaitingCounterController.java
+++ src/main/java/org/springframework/samples/petclinic/vet/WaitingCounterController.java
@@ -1,6 +1,7 @@
 package org.springframework.samples.petclinic.vet;
 
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,15 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/waiting")
 class WaitingCounterController {
 
-	private final WaitingCounterService waitingCounterService;
+    private final WaitingCounterService waitingCounterService;
+    private final VetRepository vetRepository;
 
-	WaitingCounterController(WaitingCounterService waitingCounterService) {
-		this.waitingCounterService = waitingCounterService;
-	}
+    WaitingCounterController(WaitingCounterService waitingCounterService, VetRepository vetRepository) {
+        this.waitingCounterService = waitingCounterService;
+        this.vetRepository = vetRepository;
+    }
 
-	@GetMapping
-	Map<Integer, Integer> getAllCounts() {
-		return waitingCounterService.getAllCounts();
-	}
+    @GetMapping
+    Map<Integer, Integer> getAllCounts() {
+        Map<Integer, Integer> counts = new ConcurrentHashMap<>(waitingCounterService.getAllCounts());
+        vetRepository.findAll().forEach(vet -> counts.putIfAbsent(vet.getId(), 0));
+        return counts;
+    }
 
 }
