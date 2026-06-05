bcd3b626b7d0b6f68272eeb507a80f3b63f80ad7
echosandesh <echosandeshio@gmail.com>
2026-06-05 08:14:20 +0000

    fix formatting error

--- src/main/java/org/springframework/samples/petclinic/vet/WaitingCounterController.java
+++ src/main/java/org/springframework/samples/petclinic/vet/WaitingCounterController.java
@@ -11,19 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/waiting")
 class WaitingCounterController {
 
-    private final WaitingCounterService waitingCounterService;
-    private final VetRepository vetRepository;
+	private final WaitingCounterService waitingCounterService;
 
-    WaitingCounterController(WaitingCounterService waitingCounterService, VetRepository vetRepository) {
-        this.waitingCounterService = waitingCounterService;
-        this.vetRepository = vetRepository;
-    }
+	private final VetRepository vetRepository;
 
-    @GetMapping
-    Map<Integer, Integer> getAllCounts() {
-        Map<Integer, Integer> counts = new ConcurrentHashMap<>(waitingCounterService.getAllCounts());
-        vetRepository.findAll().forEach(vet -> counts.putIfAbsent(vet.getId(), 0));
-        return counts;
-    }
+	WaitingCounterController(WaitingCounterService waitingCounterService, VetRepository vetRepository) {
+		this.waitingCounterService = waitingCounterService;
+		this.vetRepository = vetRepository;
+	}
+
+	@GetMapping
+	Map<Integer, Integer> getAllCounts() {
+		Map<Integer, Integer> counts = new ConcurrentHashMap<>(waitingCounterService.getAllCounts());
+		vetRepository.findAll().forEach(vet -> counts.putIfAbsent(vet.getId(), 0));
+		return counts;
+	}
 
 }
